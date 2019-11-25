1f5115f64a88f008bf5521d796da98745a3515c6
ajbattah <anas.battah@rankedgaming.com>
2019-11-25 22:16:41 +0200

    added new job and projects to portfolio

--- inc/sendEmail.php
+++ inc/sendEmail.php
@@ -50,7 +50,7 @@ if($_POST) {
       $mail = mail($siteOwnersEmail, $subject, $message, $headers);
 
 		if ($mail) { echo "OK"; }
-        else { echo "Something went wrong. Please try again. daniel test";}
+        else { echo "Something went wrong. Please try again.";}
 		
 	} # end if - no validation error
 
